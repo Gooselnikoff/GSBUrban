5ed1d31d8d6a0a267219d7b8c50e320f63b8454f
Gooselnikoff <Gooselnikoff@gmail.com>
2024-09-09 03:48:09 +0500

    Домашняя работа по уроку "Функции в Python.Функция с параметром"

---
+++ module_2_5.py
@@ -0,0 +1,24 @@
+# Онлайн-университет URBAN
+# Домашняя работа по уроку "Функции в Python.Функция с параметром"
+
+def get_matrix(n, m, value):
+    matrix = []
+    #n = n - 1
+    #m = m - 1
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append([j])
+            matrix[i][j] = value
+    return matrix
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+result4 = get_matrix(1, 3, 'УРА!!!')
+result5 = get_matrix(1, 1, 'ПЛБЕДА!!!')
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
