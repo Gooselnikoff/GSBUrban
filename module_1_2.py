f1011807fa01b88957a6d3756d516e7d05257592
Gooselnikoff <Gooselnikoff@gmail.com>
2024-09-11 10:25:43 +0500

    1.2 Practical task on the topic "Variables".

---
+++ module_1_2.py
@@ -0,0 +1,11 @@
+# Онлайн-университет URBAN
+# Практическое задание по теме "Переменные".
+
+number_of_completed_tasks = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_hours_spent / number_of_completed_tasks
+print ('Курс: ' + course_name + ', всего задач:' + str(number_of_completed_tasks) + ', затрачено часов:' + str(number_of_hours_spent) + ', среднее время выполнения ' + str(time_for_one_task) + ' часа.')
+
+# Вариант с f-строкой
+print (f'Курс: {course_name}, всего задач: {number_of_completed_tasks}, затрачено часов: {number_of_hours_spent}, среднее время выполнения {time_for_one_task} часа.')
