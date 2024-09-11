86f1ab53b0230eedfc12f95e3456f6cc8dc2a7ca
Gooselnikoff <Gooselnikoff@gmail.com>
2024-09-11 10:20:45 +0500

    1.1 Practical task for the lesson "Strings and string indexing"

---
+++ module_1_1.py
@@ -0,0 +1,9 @@
+# Онлайн-университет URBAN
+# Практическое задание по уроку "Строки и индексация строк"
+
+example = 'Python rules'
+print (example[0])
+print (example[-1])
+print (example[-5:])
+print (example[::-1])
+print (example[0::2])
