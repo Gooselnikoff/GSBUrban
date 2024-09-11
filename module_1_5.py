60c1560e4bef6e7d41495369700855479f50145b
Gooselnikoff <Gooselnikoff@gmail.com>
2024-09-11 10:09:51 +0500

    1.5 Practical task on the topic: "Immutable and mutable objects. Tuples"

--- module_1_5.py
+++ module_1_5.py
@@ -1,6 +1,12 @@
-example = 'Python rules'
-print (example[0])
-print (example[-1])
-print (example[-5:])
-print (example[::-1])
-print (example[1::2])
+# Онлайн-университет URBAN
+# Практическое задание по теме: "Неизменяемые и изменяемые объекты. Кортежи"
+
+immutable_var = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 7, True)
+print(immutable_var)
+#immutable_var[2] = 'Mon'
+#print(immutable_var)
+# Изменить значения элементов кортежа нельзя потому, что объект кортеж не поддерживает назначение элемента
+
+mutable_list = ['Урок истории', 10, 00, True]
+mutable_list[0] = 'Урок математики'
+print(mutable_list)
